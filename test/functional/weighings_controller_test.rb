f6298a550b068fc7e1eaba6f81429d80936747cb
Ben Symonds <ben@texperts.com>
2011-01-12 21:30:13 +0000

    remove unused routes tests

--- test/functional/weighings_controller_test.rb
+++ test/functional/weighings_controller_test.rb
@@ -21,29 +21,7 @@ class WeighingsControllerTest < ActionController::TestCase
       post :create, :weighing => @weighing.attributes
     end
 
-    assert_redirected_to weighing_path(assigns(:weighing))
-  end
-
-  test "should show weighing" do
-    get :show, :id => @weighing.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @weighing.to_param
-    assert_response :success
-  end
-
-  test "should update weighing" do
-    put :update, :id => @weighing.to_param, :weighing => @weighing.attributes
-    assert_redirected_to weighing_path(assigns(:weighing))
-  end
-
-  test "should destroy weighing" do
-    assert_difference('Weighing.count', -1) do
-      delete :destroy, :id => @weighing.to_param
-    end
-
     assert_redirected_to weighings_path
   end
+
 end
