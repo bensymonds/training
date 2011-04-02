2f20af4922df53152860a460cb7fe99bbb48f214
Ben Symonds <ben@texperts.com>
2011-04-02 15:26:16 +0100

    break down sports into more types

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
   belongs_to :repeat_activity
-  SPORTS = %w( swim cycle run row )
+  SPORTS = %w( swim cycle run treadmill erg turbo )
   validates_inclusion_of :sport, :in => SPORTS, :unless => :repeat_activity
   validates_presence_of :distance, :unless => :repeat_activity
   validates_presence_of :date
