6f2dbdeb051633ee154a224a1f99cc0bc6b0735f
Ben Symonds <ben@texperts.com>
2011-04-02 23:57:34 +0100

    remove xml format

--- app/controllers/weighings_controller.rb
+++ app/controllers/weighings_controller.rb
@@ -2,33 +2,18 @@ class WeighingsController < ApplicationController
 
   def index
     @weighings = Weighing.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @weighings }
-    end
   end
 
   def new
     @weighing = Weighing.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @weighing }
-    end
   end
 
   def create
     @weighing = Weighing.new(params[:weighing])
-
-    respond_to do |format|
-      if @weighing.save
-        format.html { redirect_to(weighings_url, :notice => 'Weighing was successfully created.') }
-        format.xml  { render :xml => @weighing, :status => :created, :location => @weighing }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @weighing.errors, :status => :unprocessable_entity }
-      end
+    if @weighing.save
+      redirect_to(weighings_url, :notice => 'Weighing was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
