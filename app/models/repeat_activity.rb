67eff0ce9213451d22edfde72e23e5f6629a5c4b
Ben Symonds <ben@texperts.com>
2011-05-14 21:48:09 +0100

    rewrite validations to use new syntax, and add a few

--- app/models/repeat_activity.rb
+++ app/models/repeat_activity.rb
@@ -1,7 +1,14 @@
 class RepeatActivity < ActiveRecord::Base
-  validates_inclusion_of :sport, :in => Activity::SPORTS
-  validates_presence_of :name, :description
-  validates_presence_of :distance, :unless => :duration
-  validates_presence_of :duration, :unless => :distance
+  validates :sport, :inclusion => {:in => Activity::SPORTS}
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :distance, :presence => true, :numericality => {:greater_than => 0}, :unless => :duration
+  validates :duration, :presence => true, :numericality => {:only_integer => true, :greater_than => 0}, :unless => :distance
+  validate :not_both_duration_and_distance
   has_many :activities
+  
+  def not_both_duration_and_distance
+    self.errors.add(:base, "Can't have both duration and distance") if self.duration && self.distance
+  end
+
 end
